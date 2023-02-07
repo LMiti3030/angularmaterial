85d1d3d3ceaa86ec5fd60806845ef7a79b6ffa4d
LMiti3030 <mititelu.laura@yahoo.com>
2023-02-07 18:31:01 +0200

    adding flex layout

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { MaterialModule } from '../shared/material.module';
-import { FormsModule } from '@angular/forms';
+
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 
 @NgModule({
   declarations: [
-    ButtonsComponent
+    ButtonsComponent,
+    FlexboxComponent
   ],
   imports: [
     CommonModule,
     DemoRoutingModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
   ]
 })
 export class DemoModule { }
