e7bfb66d57a0afa57845edd90fbb693cdfca1187
LMiti3030 <mititelu.laura@yahoo.com>
2023-02-09 18:43:34 +0200

    adding data tables  with pagination and filtering to note tab

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,13 +1,40 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.scss']
+  styleUrls: ['./notes.component.scss'],
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit, AfterViewInit {
+  @Input() notes!: Note[];
 
-  @Input() notes !: Note[];
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource!: MatTableDataSource<Note>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
